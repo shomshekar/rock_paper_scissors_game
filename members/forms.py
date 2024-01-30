2003e4f1bc6708fba338d5f9cb500d546bfe38f2
Shomshekar Shilvant <shomshekar@rediff.com>
2024-01-30 16:46:25 +0530

    Added bootstrap 5 UI as much as possible

--- members/forms.py
+++ members/forms.py
@@ -5,6 +5,20 @@ class FormName(forms.Form):
     firstname = forms.CharField()
     lastname = forms.CharField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['style'] = 'width:auto;display:inline-block;'
+            
+
+
 class FormSelect(forms.Form):
     CHOICES = (('rock', 'Rock'),('paper', 'Paper'),('sciccers', 'Sciccers'),)
     choice = forms.ChoiceField(choices=CHOICES)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-select'
+            visible.field.widget.attrs['style'] = 'width:auto;display:inline-block;'
